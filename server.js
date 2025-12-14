ec7ef9ab5d154a3ed64c86760bc456efa192cc37
aliibrhemqannan <aliibrhemqannan@gmail.com>
2025-12-14 16:46:25 +0000

    server testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Express! in  software  company');
+  res.send('Hello World from Express!  CI/CD system');
 });
 
 app.listen(port, () => {
